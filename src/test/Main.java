0a28475a51f1570203cf6be43a17531cdf7b8ff1
d1fficult <dayn2803@gmail.com>
2023-06-09 20:00:00 +0300

    fafa

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,9 +2,10 @@ package test;
 
 import example.*;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-//        example.Human human1 = new example.Human();
+    //        example.Human human1 = new example.Human();
 //        example.Human human2 = new example.Human();
 //        example.Human human3 = new example.Human();
 //        human1.age = 16;
@@ -52,9 +53,81 @@ public class Main {
 //        Human human = new Human("Jonh", 25);
 //        human.setAge(-49);
 //        System.out.println(human.getAge() + human.getName());
-        System.out.println(MyMath.length(10));
-        System.out.println(MyMath.area(10));
-        System.out.println(MyMath.length(10));
-        System.out.println(MyMath.sum(5, 5, 5, 5, 5, 5, 111));
+//        System.out.println(MyMath.length(10));
+//        System.out.println(MyMath.area(10));
+//        System.out.println(MyMath.length(10));
+//        System.out.println(MyMath.sum(5, 5, 5, 5, 5, 5, 111));
+
+    //        for (int i =0; i < employees.length; i++){
+//            newArray[i] = employees[i];
+//        }
+//        newArray[newArray.length - 1] = "James";
+//        employees = newArray;
+//        employees[0] = null;
+//        String[] newestArray = new String[employees.length - 1];
+//        for (int i = 0, j = 0; i<employees.length; i++){
+//            String employee = employees[i];
+//            if (employee != null){
+//                newestArray[j] = employee;
+//                j++;
+//            }
+//        }
+//        employees = newestArray;
+//    }
+    public static void main(String[] args) {
+//        ArrayList<String> employees = getEmployees();
+//        employees.add("James");
+//        employees.remove("Black");
+//        employees.remove("Nigger");
+//        employees.remove(0);
+//        for (String employee : employees) {
+//            System.out.println(employee);
+//        }
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 1000; i++){
+            numbers.add(i);
+        }
+        for(int number : numbers){
+            System.out.println(number);
+        }
     }
-}
+    private static ArrayList<String> getEmployees(){
+        ArrayList <String> employees = new ArrayList<>();
+        employees.add("Person");
+        employees.add("Nigger");
+        employees.add("Literally");
+        employees.add("Black");
+        employees.add("White");
+        return employees;
+//    public static void main(String[] args) {
+//        int a = 5;
+//        Integer a1 = 5;
+//        byte b;
+//        Byte b1;
+//        short c;
+//        Short c1;
+//        long d;
+//        Long d1;
+//        char e;
+//        Character e1;
+//        float g;
+//        Float g1;
+//        double h;
+//        Double h1;
+//        boolean i;
+//        Boolean i1;
+//        String s = "1000";
+//        String s1 = "2000";
+//        int a = Integer.parseInt(s);
+//        int a1 = Integer.parseInt(s1);
+//        System.out.println(s + s1);
+//        System.out.println(a + a1);
+//        String s = "This is John. He is 27 years old.";
+//        String name = s.substring(8, 12);
+//        String age = s.substring(20, 22);
+//        int age1 = Integer.parseInt(age);
+//        System.out.println(name + " " + age1);
+//        Human John = new Human(name, age1);
+//        System.out.println(John.getAge() + " " + John.getName());
+//    }
+}}
